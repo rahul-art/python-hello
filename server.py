182f5bb89c93b05ff752562145f5e50fed539785
RAHUL Danu <56379013+rahul-art@users.noreply.github.com>
2021-11-07 14:58:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from wsgiref.simple_server import make_server
+"""from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 import os
@@ -17,4 +17,26 @@ if __name__ == '__main__':
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+    server.serve_forever()"""
+    
+from flask import Flask, Response
+import cv2
+app = Flask(__name__)
+video = cv2.VideoCapture(0)
+@app.route('/')
+def index():
+    return "Default Message"
+def gen(video):
+    while True:
+        success, image = video.read()
+        ret, jpeg = cv2.imencode('.jpg', image)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@app.route('/video_feed')
+def video_feed():
+    global video
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2204, threaded=True)
