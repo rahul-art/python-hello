f51980e8f010016c9289f6a7094f204ceed5eb4e
RAHUL Danu <56379013+rahul-art@users.noreply.github.com>
2021-11-10 18:15:04 +0530

    Update process.py

--- process.py
+++ process.py
@@ -96,4 +96,4 @@ def video_feed():
 	return Response(pyshine_process(params),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=True,port=8080,threaded=True)
+    app.run(debug=True,threaded=True)
